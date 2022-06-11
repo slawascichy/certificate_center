116157fd5a53a6871cd6ecb751c11f839cf39811
Sławomir 'slawas' Cichy <scichy@scisoftware.pl>
2022-06-11 12:45:53 +0200

    Poprzednie generowanie CA było nieporozumieniem

--- generate_root_ca.sh
+++ generate_root_ca.sh
@@ -12,6 +12,19 @@ export CURR_DATE
 OUTPUT_DIR=$CA_DIR
 export OUTPUT_DIR
 
+CA_CERT_FILE=scisoftware_root_ca.crt
+CA_KEY_FILE=scisoftware_root_ca.pem
+
+#CN=$1
+CN="Sci Software Root CA"
+export CN
+
+CA_CONF=$CONFIG_DIR/002_root_ca_openssl.cnf
+export CA_CONF
+
+DAYS=7300 #20 lata
+export DAYS
+
 if [ -f "$CA_DIR/$CA_CERT_FILE" ]
 then
  #Robię kopie bezpieczenstwa starego certyfikatu...
@@ -20,15 +33,8 @@ then
  cp "$CA_DIR/$CA_CERT_FILE" "$CA_DIR/$CURR_DATE.$CA_CERT_FILE"
 fi
 
-#CN=$1
-CN="Sci Software Private Certification Authority, domain PL"
-export CN
-
-DAYS=7300 #20 lata
-export DAYS
-
 openssl genrsa $KEY_SIZE > "$CA_DIR/$CA_KEY_FILE"
-openssl req -new -x509 -nodes -key "$CA_DIR/$CA_KEY_FILE" -days $DAYS -config $MAIN_CONF > "$CA_DIR/$CA_CERT_FILE"
+openssl req -new -x509 -nodes -key "$CA_DIR/$CA_KEY_FILE" -days $DAYS -config $CA_CONF > "$CA_DIR/$CA_CERT_FILE"
 
 # Zmieniam uprawnienia do plików z certyfikatami CA
 chmod 600 $CA_DIR/$CA_KEY_FILE
