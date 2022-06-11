e7abe6fb89151dad4e208d3582cfafea489b9776
Sławomir 'slawas' Cichy <scichy@scisoftware.pl>
2022-06-11 18:43:08 +0200

    Poprawiona generacja certów serwerów.

--- generate_server_cert.sh
+++ generate_server_cert.sh
@@ -21,6 +21,11 @@ OPENSSL_CONF=$MAIN_CONF
 CONFIG_FILE_NAME="$OUTPUT_CN_DIR"
 CONFIG_FILE_NAME+=_openssl.cnf
 
+KEY_FILE=server-cert.key
+CSR_FILE=server-cert.csr
+CERT_FILE=server-cert.crt
+PCKS12_FILE=server-cert.p12
+
 if test -f "$CONFIG_DIR/$CONFIG_FILE_NAME"; then
    # Ktoś przygotował indywidualny plik konfiguracyjny 
    echo "Use custom config file $CONFIG_DIR/$CONFIG_FILE_NAME"
@@ -29,28 +34,34 @@ fi
 
 
 echo "Start key generation..."
-openssl genrsa -out $OUTPUT_DIR/server-key.pem 2048
+openssl genrsa -out $OUTPUT_DIR/$KEY_FILE 2048
 
 echo "Start request generation..."
-openssl req\
+openssl req -new -sha256 -nodes\
  -config $OPENSSL_CONF\
- -new -key $OUTPUT_DIR/server-key.pem\
- -out $OUTPUT_DIR/server-req.pem
+ -key $OUTPUT_DIR/$KEY_FILE\
+ -out $OUTPUT_DIR/$CSR_FILE
 
 echo "Start certificate generation..."
 openssl ca\
  -config $MAIN_CONF\
- -in $OUTPUT_DIR/server-req.pem\
+ -extensions srv_cert\
  -days $DAYS\
- -out $OUTPUT_DIR/server-cert.pem
-
+ -in $OUTPUT_DIR/$CSR_FILE\
+ -out $OUTPUT_DIR/$CERT_FILE
+ 
 echo "Start to create PKCS12..."
 openssl pkcs12 -export\
- -out $OUTPUT_DIR/certyfikat.p12\
- -inkey $OUTPUT_DIR/server-key.pem\
- -in $OUTPUT_DIR/server-cert.pem\
  -name "$CN"\
+ -out $OUTPUT_DIR/$PCKS12_FILE\
+ -inkey $OUTPUT_DIR/$KEY_FILE\
+ -in $OUTPUT_DIR/$CERT_FILE\
  -certfile $CA_DIR/$CA_CERT_FILE
 
-$TAR_CMD -czvf $TARGET_DIR/$OUTPUT_CN_DIR-certs.tar.gz $OUTPUT_DIR/*.p??
-echo "SUCCESS! Certs are available in archive file $TARGET_DIR/$OUTPUT_CN_DIR-certs.tar.gz"
+$TAR_CMD --append --file=$TARGET_DIR/$OUTPUT_CN_DIR-certs.tar -C $OUTPUT_DIR $KEY_FILE
+$TAR_CMD --append --file=$TARGET_DIR/$OUTPUT_CN_DIR-certs.tar -C $OUTPUT_DIR $PCKS12_FILE
+$TAR_CMD --append --file=$TARGET_DIR/$OUTPUT_CN_DIR-certs.tar -C $OUTPUT_DIR $CERT_FILE 
+$TAR_CMD --append --file=$TARGET_DIR/$OUTPUT_CN_DIR-certs.tar -C $CA_DIR $CA_ROOT_FILE
+$TAR_CMD --append --file=$TARGET_DIR/$OUTPUT_CN_DIR-certs.tar -C $CA_DIR $CA_CERT_FILE
+
+echo "SUCCESS! Certs are available in archive file $TARGET_DIR/$OUTPUT_CN_DIR-certs.tar"
--- setenv.sh
+++ setenv.sh
@@ -14,12 +14,13 @@ export TAR_CMD
 
 # Katalog z certyfikatem organizacji podpisującej
 CA_DIR=./cacerts
-CA_CERT_FILE=scisoftware_root_ca.crt
-CA_KEY_FILE=scisoftware_root_ca.pem
+CA_ROOT_FILE=scisoftware_root_ca.crt
+CA_CERT_FILE=scisoftware_intermediate_ca.crt
+CA_KEY_FILE=scisoftware_intermediate_ca.pem
 CONFIG_DIR=./config
 TARGET_DIR=./target
 DATABASE_DIR=./database
-export CA_DIR CA_CERT_FILE CA_KEY_FILE CONFIG_DIR TARGET_DIR DATABASE_DIR
+export CA_ROOT_FILE CA_DIR CA_CERT_FILE CA_KEY_FILE CONFIG_DIR TARGET_DIR DATABASE_DIR
 
 # Czas ważności certyfikatu
 DAYS=1095 #3 lata
@@ -30,5 +31,5 @@ KEY_SIZE=2048
 export KEY_SIZE
 
 # Główny plik konfiguracyjny. Nazwanu z prefiksem 001 aby był na początku.
-MAIN_CONF=$CONFIG_DIR/001_main_openssl.cnf
+MAIN_CONF=$CONFIG_DIR/003_intermediate_ca_openssl.cnf
 export MAIN_CONF
